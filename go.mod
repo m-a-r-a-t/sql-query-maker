1821e7bbbe59c0a6787d222e364a485b02729b2d
s.marat <razorlevel@gmail.com>
2023-11-03 11:48:59 +0300

    added pq.Array support

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/m-a-r-a-t/sql-query-maker
 
 go 1.17
+
+require github.com/lib/pq v1.10.9
--- maker.go
+++ maker.go
@@ -2,6 +2,8 @@ package sql_query_maker
 
 import (
 	"fmt"
+	"github.com/lib/pq"
+	"reflect"
 	"strings"
 )
 
@@ -127,6 +129,12 @@ func (q *SqlQueryMaker) Args() []interface{} {
 
 // Make return query and args
 func (q *SqlQueryMaker) Make() (string, []interface{}) {
+	for i := 0; i < len(q.args); i++ {
+		if reflect.TypeOf(q.args[i]).Kind() == reflect.Slice {
+			q.args[i] = pq.Array(q.args[i])
+		}
+	}
+
 	return q.query.String(), q.args
 }
 
